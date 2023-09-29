574cfead27b09c76cbb4a19d58936c9df272deaa
AnaBaccaro <analaurabaccaro@gmail.com>
2023-09-28 22:14:02 -0300

    actualicé entrega

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-class Producto {
-    constructor(id, nombre, precio, descripcion, img, alt, categoria, cantidad = 1) {
+class Producto { 
+    constructor(id, nombre, precio, descripcion, img, alt, categoria, stock, cantidad = 1) {
         this.id = id;
         this.nombre = nombre;
         this.precio = precio;
@@ -8,6 +8,7 @@ class Producto {
         this.img = img;
         this.alt = alt;
         this.categoria = categoria;
+        this.stock = stock;
     }
 
     //Aumentar y dismuir las cantidades de los productos en el carrito
@@ -21,6 +22,19 @@ class Producto {
         }
     }
 
+    estaAgotado() {
+        return this.stock <= 0
+    }
+    
+    actualizarStock(cantidad) {
+        const stockDisponible = this.stock >= this.cantidad
+        if (stockDisponible) {
+            this.stock -= cantidad
+        }
+        return stockDisponible
+        }
+    
+
     //Modal carrito
     descripcionCarrito(){
         return `
@@ -33,12 +47,12 @@ class Producto {
                     <div class="card-body">
                         <h5 class="card-title">${this.nombre}</h5>
                         <p class="card-text">Cantidad:
-                        <button class="btn btn-dark" id="disminuir-${this.id}"><i class="fa-solid fa-minus"></i></button>
-                        ${this.cantidad}
-                        <button class="btn btn-dark" id="aumentar-${this.id}"><i class="fa-solid fa-plus"></i></button>
+                        <button class="btn btn-cantidad" id="disminuir-${this.id}"><i class="fa-solid fa-minus"></i></button>
+                        ${this.cantidad > 0 ? this.cantidad : "Agotado"}
+                        <button class="btn btn-cantidad" id="aumentar-${this.id}"><i class="fa-solid fa-plus"></i></button>
                         </p>
                         <p class="card-text">Precio: $${this.precio * this.cantidad}</p>
-                        <button id = "eliminarprod-${this.id}">
+                        <button class="btn-eliminar mt-5" id = "eliminarprod-${this.id}">
                             <i class="fa-solid fa-trash"></i>
                         </button>                    
                     </div>
@@ -50,13 +64,13 @@ class Producto {
     //Card productos
     descripcionProducto(){
         return `
-        <div class="card border-light" style="width: 18rem;">
-            <img src="${this.img}" class="card-img-top" alt="${this.alt}">
+        <div class="card border-light custom-card mx-auto" style="width: 18rem;">
+            <img src="${this.img}" class="card-img-top card_img" alt="${this.alt}">
             <div class="card-body">
-                <h5 class="card-title">${this.nombre}</h5>
-                <p class="card-text">${this.descripcion}</p>
-                <p class="card-text">$${this.precio}</p>
-                <button class="btn btn-cart" id="ap-${this.id}">Añadir al carrito</button>
+                <h5 class="card-title fw-bold nombre_producto">${this.nombre}</h5>
+                <p class="card-text descripcion_producto">${this.descripcion}</p>
+                <p class="card-text fw-bold descripcion_precio">USD $${this.precio}</p>
+                <button class="btn btn-cart" data-product-id="${this.id}">Añadir al carrito</button>
             </div>
         </div>`
     }
@@ -73,82 +87,108 @@ class ProductoController {
         }
     }
 
-    //Productos disponibles y sus propiedades
-    cargarProductos(){
+    //Productos que provienen de la API creada
+     async preparar_contenedor_productos(){
+        this.listaProductosJSON = await fetch("productos.json")
+        let listaProductosJS = await this.listaProductosJSON.json()
 
-        this.agregar( new Producto (1, "Lentes de sol Almond",150, "Lentes de sol vintage, hechos en Italia", "https://images.pexels.com/photos/9771805/pexels-photo-9771805.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", "Lentes_de_sol", "Accesorios") )
-        this.agregar(new Producto(2, "Camisa Mercury",60, "Camisa denim manga corta, realizada con desechos de fábrica", "https://images.pexels.com/photos/12064935/pexels-photo-12064935.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", "Camisa_denim", "Ropa") )
-        this.agregar(new Producto(3, "Tshirt Smoky" ,45, "Tshirt dont smoke", "https://images.pexels.com/photos/14711713/pexels-photo-14711713.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", "Tshirt_estampada", "Ropa") )
-        this.agregar(new Producto(4, "Tshirt Donsmoke",45, "Tshirt oversize", "https://images.pexels.com/photos/14769304/pexels-photo-14769304.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", "Tshirt_oversize", "Ropa") )
-        this.agregar(new Producto(5, "Tshirt Marie",45, "Tshirt fucsia con estampa", "https://images.pexels.com/photos/14845062/pexels-photo-14845062.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", "Tshirt_estampa", "Ropa") )
-        this.agregar(new Producto(6, "Campera Fuji",100, "Campera bordada", "https://images.pexels.com/photos/14844862/pexels-photo-14844862.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", "Campera_bordada", "Ropa") )
-        this.agregar(new Producto(7, "Tshirt Ozzy",100, "Tshirt con estampa de Ozzy Osbourne", "https://images.pexels.com/photos/11131439/pexels-photo-11131439.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", "Tshirt_ozzy", "Ropa") )
-        this.agregar(new Producto(8, "Lentes de sol Ducky",150, "Lentes de sol vintage, hechos en España", "https://images.pexels.com/photos/14700245/pexels-photo-14700245.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", "Lentes_vintage", "Accesorios") )
-        this.agregar(new Producto(9, "Cardigan Justin",80, "Saco vintage a rayas", "https://images.pexels.com/photos/14844788/pexels-photo-14844788.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load", "Cardigan_vintage", "Ropa") )
-        this.agregar(new Producto(10, "Hoodie D-Smoke",100, "Hoodie con estampa", "https://images.pexels.com/photos/14769361/pexels-photo-14769361.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load", "Hoodie_estampado", "Ropa") )
-        this.agregar(new Producto(11, "Buzo Wulf", 100, "Buzo crop con cuello alto", "https://images.pexels.com/photos/14711368/pexels-photo-14711368.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load", "Buzo_vintage", "Ropa") )
-        this.agregar(new Producto(12, "Botas Matrix",150, "Botas vintage", "https://images.pexels.com/photos/14700242/pexels-photo-14700242.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load", "Botas_vintage", "Zapatos") )
+        listaProductosJS.forEach(producto => {
+            let nuevoProducto = new Producto(producto.id, producto.nombre, producto.precio, producto.descripcion, producto.img, producto.alt, producto.categoria, producto.stock, producto.cantidad)
+            this.agregar(nuevoProducto)
+        })
+
+        this.mostrarEnDOM()
     }
 
     mostrarEnDOM(categoriaSeleccionada) {
-        let contenedor_productos = document.getElementById("contenedor_productos");
-        contenedor_productos.innerHTML = "";
-    
+        let contenedor_productos = document.getElementById("contenedor_productos")
+        let productosFiltrados = []
+
         this.listaProductos.forEach(producto => {
             if (!categoriaSeleccionada || producto.categoria === categoriaSeleccionada) {
-                contenedor_productos.innerHTML += producto.descripcionProducto();
+                productosFiltrados.push(producto)
             }
         })
 
-        
+        contenedor_productos.innerHTML = ""
 
-        this.listaProductos.forEach(producto => {
-            const btn_ap = document.getElementById(`ap-${producto.id}`)
+        productosFiltrados.forEach(producto => {
+            contenedor_productos.innerHTML += producto.descripcionProducto()
+        })
 
-            btn_ap.addEventListener("click",()=>{
+    const btns_ap = document.querySelectorAll('.btn-cart')
+
+    btns_ap.forEach((btn) => {
+        const productoId = btn.getAttribute("data-product-id")
+        const producto = productosFiltrados.find((p) => p.id === parseInt(productoId))
+
+
+            btn.addEventListener("click", () => {
+                if (producto.stock > 0){
                 carrito.agregar(producto)
                 carrito.guardarEnStorage()
                 carrito.mostrarEnDOM()
+                carrito.actualizarCantidadCarrito()
+                carrito.actualizarCantidadCarritoEnIcono()
+            } else {
+                carrito.mostrarToastifyStock(producto)
+            }
             })
         })
+    
+
+    carrito.actualizarCantidadCarrito()
+        const carritoCount = document.getElementById("carrito-count");
+        const cantidadEnCarrito = carrito.listaCarrito.reduce((total, producto) => total + producto.cantidad, 0)
+        carritoCount.textContent = cantidadEnCarrito.toString();
     }
 }
 
+
 class Carrito {
     constructor() {
-        this.listaCarrito = []
+        this.localStorageKey = "listaCarrito"
+        this.listaCarrito = JSON.parse(localStorage.getItem(this.localStorageKey)) || []
     }
 
     agregar(productoAgregar) {
-        let existe = this.listaCarrito.some(producto => producto.id == productoAgregar.id);
+        const productoId = productoAgregar.id
+        let existe = this.listaCarrito.find(producto => producto.id == productoId)
     
         if (existe) {
-            let producto = this.listaCarrito.find(producto => producto.id == productoAgregar.id);
-            producto.aumentarCantidad();
-            // Actualiza el precio total del producto
-            producto.precioTotal = producto.precio * producto.cantidad;
-        } else {
-            if (productoAgregar instanceof Producto) {
-                this.listaCarrito.push(productoAgregar);
-                productoAgregar.precioTotal = productoAgregar.precio;
+            const cantidad = productoAgregar.cantidad
+    
+            if (existe.actualizarStock(cantidad)) {
+                existe.aumentarCantidad(cantidad)
+                existe.precioTotal = existe.precio * existe.cantidad
+            } else {
+                this.mostrarToastifyStock()
+            }
+        } else if (productoAgregar instanceof Producto) {
+            if (productoAgregar.actualizarStock(1)) {
+                this.listaCarrito.push(productoAgregar)
+                productoAgregar.precioTotal = productoAgregar.precio
+            } else {
+                this.mostrarToastifyStock()
             }
         }
+        this.guardarEnStorage()
     }
-    
-
-    
+        
     eliminar(productoAeliminar){
         let indice = this.listaCarrito.findIndex(producto => producto.id == productoAeliminar.id)
         this.listaCarrito.splice(indice,1)
+
+        this.guardarEnStorage()
     }
 
     guardarEnStorage(){
         let listaCarritoJSON = JSON.stringify(this.listaCarrito)
-        localStorage.setItem("listaCarrito", listaCarritoJSON)
+        localStorage.setItem(this.localStorageKey, listaCarritoJSON)
     }
 
     recuperarStorage(){
-        let listaCarritoJSON = localStorage.getItem("listaCarrito")
+        let listaCarritoJSON = localStorage.getItem(this.localStorageKey)
         let listaCarritoJS = JSON.parse(listaCarritoJSON)
         let listaAux = []
         listaCarritoJS.forEach( producto => {
@@ -167,10 +207,11 @@ class Carrito {
             contenedor_carrito.innerHTML += producto.descripcionCarrito()
         })
     
-        this.eventoEliminar();
-        this.eventoAumentarCantidad();
-        this.eventoDisminuirCantidad();
-        this.mostrarTotal();
+        this.eventoEliminar()
+        this.eventoAumentarCantidad()
+        this.eventoDisminuirCantidad()
+        this.mostrarTotal()
+        this.actualizarCantidadCarritoEnIcono()
     }
     
 
@@ -181,6 +222,7 @@ class Carrito {
                 this.eliminar(producto)
                 this.guardarEnStorage()
                 this.mostrarEnDOM()
+                this.mostrarToastifyEliminar()
             })
         })
     }
@@ -204,15 +246,99 @@ class Carrito {
             })
         })
     }
+
+    calcularCantidadTotal() {
+        return this.listaCarrito.reduce((total, producto) => total + producto.cantidad, 0)
+    }
+
     
+    actualizarCantidadCarrito(){
+        const carritoCount = document.getElementById("carrito-count")
+        const cantidadEnCarrito = this.calcularCantidadTotal()
+        carritoCount.textContent = cantidadEnCarrito.toString
+    }
+
+    actualizarCantidadCarritoEnIcono() {
+        const carritoCount = document.getElementById("carrito-count")
+        const cantidadEnCarrito = this.calcularCantidadTotal()
+        carritoCount.textContent = cantidadEnCarrito.toString()
+    }
+
     calcularTotal() {
         return this.listaCarrito.reduce((total, producto) => total + producto.precio * producto.cantidad, 0)
     }
     
-
     mostrarTotal(){
         const precio_total = document.getElementById("precio_total")
         precio_total.innerText = `Precio total: $${this.calcularTotal()}`
+    }
+
+    mostrarToastifyEliminar(){
+Toastify({
+    text: "¡Producto voló del carrito! 🚀",
+    duration: 2000,
+    newWindow: true,
+    close: true,
+    gravity: "bottom", 
+    position: "right", 
+    stopOnFocus: true, 
+    style: {
+    background: "white",
+    color: "black",
+    },
+    onClick: function(){} 
+  }).showToast();
+    }
+
+    mostrarToastifyStock(){
+        Toastify({
+            text: "¡Este tesoro único encontró su dueño! \n Explora otras joyas en nuestra tienda que te encantarán 💚",
+            duration: 3000,
+            newWindow: true,
+            close: true,
+            gravity: "bottom", 
+            position: "right", 
+            stopOnFocus: true, 
+            style: { 
+            background: "white",
+            color: "black",
+            },
+            onClick: function(){} 
+          }).showToast();
+            }
+    
+    limpiarCarrito(){
+        this.listaCarrito = []
+    }
+
+    eventoFinalizarCompra(){
+        const finalizar_compra = document.getElementById("finalizar_compra")
+
+        finalizar_compra.addEventListener("click", () => {
+
+            if(this.listaCarrito.length > 0){
+                localStorage.setItem(this.listaCarritoKey, "[]")
+                this.limpiarCarrito()
+                this.mostrarEnDOM()
+
+                Swal.fire({
+                    position: 'center',
+                    icon: 'success',
+                    title: 'Compra exitosa. ¡Arrasa con tu estilo!😎',
+                    showConfirmButton: false,
+                    timer: 2000
+                })
+            } else {
+                Swal.fire({
+                    position: 'center',
+                    icon: 'warning',
+                    title: 'Parece que tu carrito está un poco solitario. 🛒🌟 Dale vida añadiendo algunos productos geniales!',
+                    showConfirmButton: false,
+                    timer: 2000
+                  })
+            }
+            
+        })
     }
 }
 
@@ -241,16 +367,20 @@ accesoriosLink.addEventListener("click", () => {
 })
 
 //Filtro de categoria de productos: Todos los productos
-const todosProductosLink = document.getElementById("todos_productos_link");
+const todosProductosLink = document.getElementById("todos_productos_link")
 
 todosProductosLink.addEventListener("click", () => {
     CP.mostrarEnDOM()
 })
 
 
-
-CP.cargarProductos()
-CP.mostrarEnDOM()
+CP.preparar_contenedor_productos()
 
 carrito.recuperarStorage()
 carrito.mostrarEnDOM()
+carrito.eventoFinalizarCompra()
+carrito.actualizarCantidadCarrito()
+carrito.actualizarCantidadCarritoEnIcono()
+
+
+
